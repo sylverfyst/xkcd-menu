2f6ce5e6d459716aea2ad621558757130fb4daff
Bryan Metzger <sylverfyst@gmail.com>
2018-11-07 14:11:29 -0500

    update to result spec to test that it correctly pretty prints the results

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -4,31 +4,39 @@ require 'bigdecimal/util'
 
 RSpec.describe Result, type: :model do
 	describe "#fancy_result" do
-		let(:item){ create(:item) }
-		let(:order){ create(:order) }
-		let(:result){ create(:result) }
 		
 		it "checks for items" do
-			expect(Item.where(name: result.names[0])).to eq(Item.find_by(name: "Pizza"))
+			item = create(:item)
+			order = create(:order)
+			result = create(:result)
+			type = result.names[0].to_s
+			temp_item = Item.where(name: type).last
+
+			expect(temp_item.attributes).to eq(Item.find_by(name: "Pizza").attributes)
 		end
 
 		it "checks for different types of menu items" do
+			item = create(:item)
+			order = create(:order)
+			result = create(:result)
 			expect(result.names.uniq).to eq(["Pizza"])
 		end
 
-		it "generates json object of menu item" do 
-			expect(Item.find_by(name: result.names[0]).select("name, cost").as_json).to eq({"id" => nil, "name" => "Pizza", "cost" => 0.215e1.to_d})
-		end
-
 		it "gets the number of different types in the order and adds them to the item object" do
+			item = create(:item)
+			order = create(:order)
+			result = create(:result)
 			item_object = Item.where(name: result.names[0]).select("name, cost").first.as_json
 			count = result.names.count(result.names[0])
 			item_object["count"] = count
+
 			expect(item_object).to eq({"id" => nil, "name" => "Pizza", "cost" => 0.215e1.to_d, "count" => 3})
 		end
 
 		it "pretty prints the order" do
-
+			item = create(:item)
+			order = create(:order)
+			result = create(:result)
 			fancy = result.fancy_result
 
 			expect(fancy).to eq([{"id" => nil, "name" => "Pizza", "cost" => 0.215e1.to_d, "count" => 3}])
